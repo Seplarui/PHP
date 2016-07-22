714b0296f84bd0307c9d5a8f8c256b3e123d4ffb
Sebas Sebas <seplarui@gmail.com>
2016-07-22 21:23:26 +0200

    Primera Subida

---
+++ index.php
@@ -0,0 +1,20 @@
+<!<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Documento sin título</title>
+    </head>
+    <body>
+
+    <?php
+    $nombre="Juan";
+
+    ?>
+        
+        
+        
+    </body>
+
+</html>
+             
+        
