541b5dbaf1e52bd577481111d9f0b87d610dab23
Sebas Sebas <seplarui@gmail.com>
2016-07-23 23:47:56 +0200

    Sabado

---
+++ ejemplo_ambitos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Documento sin título</title>
+</head>
+
+<body>
+    <?php
+
+    $nombre="Juan";
+    include("datos_otros.php");
+    dameNombre();
+	
+
+
+?>
+</body>
+
+</html>
