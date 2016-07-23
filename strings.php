587cba1e45347095410fab39d14678b8b31e13ca
Sebas Sebas <seplarui@gmail.com>
2016-07-24 00:27:16 +0200

    Strings

---
+++ strings.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Strings</title>
+
+	<style>
+
+	.resaltar {
+		color:red;
+		font-weight: bold;
+	}
+
+	</style>
+</head>
+<body>
+
+<?php 
+
+
+	$variable1="casa";
+	$variable2="CASA";
+
+
+	echo strcasecmp(variable1,variable2); //DEVUELVE 1 SI NO SON IGUALES, 0 SI SON IGUALES
+
+
+	if (!$resultado) {
+
+		echo "Coinciden";
+	} else {
+		echo "No Coinciden";
+	}
+
+
+
+ ?>
+	
+</body>
+</html>
