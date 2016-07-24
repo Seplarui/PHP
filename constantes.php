033a3085b0af72e12fc39fb630f07e613c76de42
Sebas Sebas <seplarui@gmail.com>
2016-07-24 11:55:16 +0200

    Constantes

---
+++ constantes.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Constantes</title>
+</head>
+<body>
+<?php
+
+	define("AUTOR", "Juan");
+
+	echo AUTOR;
+
+
+?>
+	
+</body>
+</html>
