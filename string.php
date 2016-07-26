25b828f5c63dfd7ec2f8fcaf28bb293ac570ae04
Sebas Sebas <seplarui@gmail.com>
2016-07-26 22:36:16 +0200

    String-Comillas

---
+++ string.php
@@ -0,0 +1,21 @@
+<?php
+
+    //Definición de variables de tipo string
+
+    $cadena1="Esto es una cadena de caracteres";
+    $cadena2='Esto es una cadena entre comillas simples';
+    $cadena3="";
+
+    $dato="IMPRIMIBLE";
+    $texto_simple='No puede imprimir la variable $dato';
+    $texto_doble="Imprime la variable dato como $dato";
+
+    echo ("$texto_simple<br>");
+    echo("$texto_doble");
+
+
+
+
+
+
+?>
