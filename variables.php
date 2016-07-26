d22a69ef019c67c5397ca1bf8dfc84553875c2f0
Sebas Sebas <seplarui@gmail.com>
2016-07-26 22:02:27 +0200

    Variables

---
+++ variables.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Variables</title>
+</head>
+<body>
+
+<?php
+	$numero_entero=10;
+
+	$NUMERO_ENTERO=20;
+
+	$numero_flotante=12343.123;
+	$cadena_caracteres="12343";
+	echo "numero_entero es y NUMERO_ENTERO son distintos<br>";
+    echo "numero_entero es $numero_entero<br>";
+    echo "NUMERO_ENTERO es $NUMERO_ENTERO<br>";
+    $verdadero=TRUE;
+    $vacio=NULL;
+    echo $verdadero;
+    echo $vacio;
+
+	
+	
+	
+	?>
+
+
+	
+</body>
+</html>
