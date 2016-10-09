8a815175c293c174ec3dbdff3f7bf322ff7118ce
Sebas Sebas <seplarui@gmail.com>
2016-10-09 10:57:08 +0200

    .\Ejercicio1-3b.php

---
+++ Ejercicio1-3b.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            PHP - Hello World
+        </title>
+    </head>
+    <body>
+        <?php
+        //LAS VARIABLES SIEMPRE EMPIEZAN CON '$'
+        $strHello="Hello";
+        /* LAS LÍNEAS DE CÓDIGO COMENTADAS PUEDEN OCUPAR 
+         MÁS DE UNA LÍNEA */
+        print($strHello." World");
+        ?>
+    </body>
+</html>
