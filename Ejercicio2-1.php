9912fbee36d111e9114674ddb09ebfdd3a61187f
Sebas Sebas <seplarui@gmail.com>
2016-10-09 11:13:42 +0200

    Ejercicio2-1.php

---
+++ Ejercicio2-1.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    <title>Ejercicio 2-1</title>
+    </head>
+    <body>
+        <?php
+        /*Hacer un programa en PHP que escriba vuestro nombre (en negrita) y la ciudad donde
+naciste y variables.*/
+        $nombre="Sebas";
+        $ciudad="Valencia";
+        
+        print("Mi nombre es: <b>$nombre</b>");
+        print("</br>");
+        print("Naci en $ciudad");
+                
+        
+        ?>
+    </body>
+</html>
