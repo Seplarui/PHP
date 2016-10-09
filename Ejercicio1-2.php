d83eddebad2df764b9e20c15eaf4a0f8e8e97b53
Sebas Sebas <seplarui@gmail.com>
2016-10-09 10:55:52 +0200

    Ejercicio1-2-3.php

---
+++ Ejercicio1-2.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>PHP-Hello World</title>
+    </head>
+    <body>
+        <?php
+        // echo y print son equivalentes
+        echo "Hello World";
+        ?>
+    </body>
+    
+    
+</html>
---
+++ Ejercicio1-3.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>
+            PHP - Hello World
+        </title>
+    </head>
+    <body>
+        <?php
+        //LAS VARIABLES SIEMPRE EMPIEZAN CON '$'
+        $strHello="Hello";
+        /* LAS LÍNEAS DE CÓDIGO COMENTADAS PUEDEN OCUPAR 
+         MÁS DE UNA LÍNEA */
+        
+        print("$strHello World!");
+        ?>
+    </body>
+</html>
