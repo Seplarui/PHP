d034b14efdc3470cdbdba233b08ce3a905393f6a
Sebas Sebas <seplarui@gmail.com>
2016-10-09 12:02:34 +0200

    Ejercicio2-2.php

---
+++ Ejercicio2-2.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Ejercicio2-2</title>
+    </head>
+    <body>
+        <?php
+        
+        $resultado=0;
+        $num1=-1;
+        $num2=9;
+        
+        print("El resultado de la suma de $num1 y $num2 es: ". $resultado=$num1+$num2);
+        
+        ?>
+    </body>
+    
+   
+</html>
+
