80bb28276579ea3ba4613e5252fcfa6baf3601f3
Sebas Sebas <seplarui@gmail.com>
2016-11-01 15:35:26 +0100

    Comparar String

---
+++ comparastring1.php
@@ -0,0 +1,27 @@
+<html>
+
+<!--Enunciado: Compara dos variables para saber si el nombre es el mismo nombre de persona.-->
+
+
+<?php
+
+
+$cadena1="Juan";
+$cadena2="Pepe";
+
+
+if ($cadena1==$cadena2) {
+
+    echo "Las dos cadenas son iguales";
+} else {
+    echo "Las dos cadenas no son iguales";
+}
+
+
+
+?>
+
+
+
+
+</html>
