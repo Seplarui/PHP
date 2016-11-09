bd8cf3acc4cf90dacdc3e2e4ea439c4ac4edeb2b
Sebas Sebas <seplarui@gmail.com>
2016-11-09 20:35:09 +0100

    DP2

--- Practica/dp1.php
+++ Practica/dp1.php
@@ -2,13 +2,5 @@
 
 $nombre=$_REQUEST['nombre'];
 
-if ($nombre=="") {
-    echo "Introduce algún dato.";
-    header("Location: http://192.168.1.45/PHP/Practica/dp1.html");
-
-} else {
     echo "El nombre introducido es: $nombre";
-}
-
-
 ?>
---
+++ Practica/dp2.php
@@ -0,0 +1,15 @@
+<?php
+
+$nombre=$_REQUEST['nombre'];
+
+if ($nombre=="") {
+    echo "Introduce algun dato.";
+    echo "<a href=dp2.html>Volver</a>";
+    //header("Location: http://192.168.1.45/PHP/Practica/dp2.html");
+
+} else {
+    echo "El nombre introducido es: $nombre";
+}
+
+
+?>
