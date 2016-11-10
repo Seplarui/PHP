1a9e60e3c1528e38c6ba971ca4e3180c2ec13ede
Sebas Sebas <seplarui@gmail.com>
2016-11-10 12:30:57 +0100

    DP5

--- Practica/dp5.php
+++ Practica/dp5.php
@@ -1,10 +1,24 @@
 <?php
+error_reporting (0);
+$varcine=$_REQUEST['cine'];
+$varliteratura=$_REQUEST['literatura'];
+$varmusica=$_REQUEST['musica'];
 
 
 
+if(!isset($varcine) && !isset($varliteratura) && !isset($varmusica)) {
 
+    echo "Ni le gusta la musica, ni el cine ni la literatura";
 
+} 
+ if ($varcine=="on") {
+    echo "Le gusta el cine";
 
-
-
+} 
+if ($varliteratura=="on") {
+    echo "Le gusta la literatura";
+} 
+if ($varmusica=="on") {
+    echo "Le gusta la musica";
+}
 ?>
