1b819bff06368722d836bce148bf689ab948303e
Sebas Sebas <seplarui@gmail.com>
2016-11-11 11:26:12 +0100

    DP1 FIN

---
+++ Practica/dp1.php
@@ -0,0 +1,8 @@
+<?php
+echo "<meta charset='utf-8'>";
+
+$nombre=$_REQUEST['nombre'];
+
+    echo "El nombre introducido es: <b>$nombre</b> <br>";
+    echo "<a href='dp1.html'>Volver a la página principal</a>";
+?>
