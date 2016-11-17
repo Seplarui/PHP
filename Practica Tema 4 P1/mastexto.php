033d6a9bf47a71be9824cf8a87c8cde14057be52
Sebas Sebas <seplarui@gmail.com>
2016-11-17 14:22:16 +0100

    Añadir líneas

---
+++ Practica Tema 4 P1/mastexto.php
@@ -0,0 +1,8 @@
+<?php
+
+$fich=fopen("leeme.txt","a"); //crea el archivo de texto leeme2.txt y lo abre en modo escritura
+fwrite($fich, "1.-Añado nueva línea de texto MIA".PHP_EOL);
+fwrite($fich, "2.-Añado una segunda nueva línea de texto MIA".PHP_EOL);
+fclose($fich); //una vez que acabo con el fichero lo cierro.
+
+?>
