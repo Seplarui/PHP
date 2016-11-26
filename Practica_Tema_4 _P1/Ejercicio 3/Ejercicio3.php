672ca99d6ddaf23c758734bbd7ecc4593de41184
Sebas <seplarui@gmail.com>
2016-11-26 19:41:41 +0100

    Ejercicio 3

---
+++ Practica_Tema_4 _P1/Ejercicio 3/Ejercicio3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio 3</title>
+</head>
+<body>
+
+<h1>Libro de Visitas</h1>
+
+<form action="comentarios.php" method="post">
+<label for="comentario">Tu Comentario</label>
+<br>
+<textarea id="comentario" name="comentario" cols="50" rows="10" maxlength="200"></textarea>
+
+<br>
+<label for="nombre">Nombre </label>
+<br>
+<input type="text" name="nombre">
+<br>
+<label for="correo">Tu correo</label>
+<br>
+<input type="text" name="email">
+<br>
+<input type="submit" Value="Enviar Comentario">
+
+
+
+</form>
+
+</body>
+</html>
---
+++ Practica_Tema_4 _P1/Ejercicio 3/comentarios.php
@@ -0,0 +1,21 @@
+<?php
+
+$nombre=$_REQUEST['nombre'];
+$correo=$_REQUEST['email'];
+$comentario=$_REQUEST['comentario'];
+
+echo "El nombre es $nombre";
+echo "<br>";
+echo "El correo es $correo";
+echo "<br>";
+echo "El comentario es $comentario";
+echo "<br>";
+
+
+
+
+
+
+
+
+?>
